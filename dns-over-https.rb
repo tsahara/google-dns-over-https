1192d07b2977a80b54661d53f834d7ff117006d8
Tomoyuki Sahara <sahara@caddr.net>
2016-06-13 14:12:20 +0900

    renamed and remove test code.

--- dns-over-https.rb
+++ dns-over-https.rb
@@ -16,12 +16,13 @@ end
 
 module DNS
   class Query
-    def initialize bytes
+    def initialize bytes, src
       @bytes = bytes
+      @src   = src
       @qname, @qtype = parse_question
     end
 
-    attr_reader :bytes, :qname, :qtype
+    attr_reader :bytes, :qname, :qtype, :src
 
     def parse_question
       labels = []
@@ -43,6 +44,7 @@ module DNS
   class Response
     def initialize query, json
       @json = json
+      @dst  = query.src
 
       qid, qword = query.bytes.unpack("nn")
 
@@ -66,7 +68,7 @@ module DNS
       @bytes = bytes
     end
 
-    attr_reader :bytes
+    attr_reader :bytes, :dst
 
     def make_question(bytes, question)
       s = ""
@@ -89,7 +91,10 @@ module DNS
         when 12 # PTR
           rdata = name2labels(a["data"])
         when 16 # TXT
-          rdata = name_to_character_string(a["data"])
+          # XXX: double quotation could be escaped (\")
+          rdata = a["data"].gsub(/"([^"]*)"/) {
+            name_to_character_string($1)
+          }
         when 28 # AAAA
           rdata = IPAddr.new(a["data"]).hton
         else
@@ -107,18 +112,46 @@ module DNS
     def initialize
       @https = Net::HTTP.new("dns.google.com", 443)
       @https.use_ssl = true
+      @https.keep_alive_timeout = 30
+      @query_queue = Queue.new
+      @response_queue = Queue.new
     end
 
-    def query(name, type)
-      @https.start { |http|
-        puts "query: /resolve?name=#{name}&type=#{type}"
-        response = http.get("/resolve?name=#{name}&type=#{type}")
-        unless response.is_a? Net::HTTPSuccess
-          raise "HTTP error: #{response.inspect}"
-        end
+    def dequeue
+      @response_queue.pop
+    end
 
-        JSON.parse(response.body)
-      }
+    def enqueue(query)
+      @query_queue.push query
+    end
+
+    def run
+      @thread = Thread.new do
+        @https.start { |http|
+          loop do
+            begin
+              query = @query_queue.pop
+              path = "/resolve?name=#{query.qname}&type=#{query.qtype}"
+
+              t = Time.now
+              response = http.get(path)
+              rtt = Time.now - t
+
+              puts "query: #{path}, rtt=#{rtt}"
+              unless response.is_a? Net::HTTPSuccess
+                raise "HTTP error: #{response.inspect}"
+              end
+
+              json = JSON.parse(response.body)
+              r = DNS::Response.new(query, json)
+              @response_queue.push r
+            rescue => e
+              puts "Exception #{e} in query thread"
+              puts e.backtrace
+            end
+          end
+        }
+      end
     end
   end
 end
@@ -127,18 +160,15 @@ udp = UDPSocket.new
 udp.setsockopt(:SOCKET, :REUSEADDR, true)
 udp.bind("127.0.0.1", 8053)
 
-dns = DNS::HTTPS.new
+Thread.abort_on_exception = true
+server = DNS::HTTPS.new
+server.run
 
 loop do
   pkt, src = udp.recvfrom(5000)
-  query = DNS::Query.new pkt
-  json = dns.query(query.qname, query.qtype)
-  p json
-  r = DNS::Response.new(query, json)
-  p r.bytes
-
-  sa = Socket.sockaddr_in(src[1], src[3])
+  query = DNS::Query.new(pkt, src)
+  server.enqueue(query)
+  r = server.dequeue
+  sa = Socket.sockaddr_in(r.dst[1], r.dst[3])
   udp.send(r.bytes, 0, sa)
 end
-
-dns.query "www.kame.net"
